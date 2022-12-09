8a6314bcfc0fec3e08070af275fc3246f6f7520e
Jeannegbl <jeanne.gablin@gmail.com>
2022-12-09 10:24:17 +0100

    refacto

--- NombreRomainEval/NombreChiffreRomain.cs
+++ NombreRomainEval/NombreChiffreRomain.cs
@@ -9,12 +9,21 @@ public class NombreChiffreRomain
     {
         return nombre switch
         {
-            1 => Unite,
-            2 => Unite + Unite,
-            3 => Unite + Unite + Unite,
+            <= 3 => NombreBoucle(nombre),
             4 => Unite + Cinq,
             5 => Cinq,
             _ => throw new NotImplementedException(),
         };
+    }
+
+    public static string NombreBoucle(int nombre)
+    {
+        var resultat_romain = "";
+        for (int i = 0; i < nombre; i++)
+        {
+            resultat_romain = resultat_romain + Unite;
+        }
+
+        return resultat_romain;
     }
 }
