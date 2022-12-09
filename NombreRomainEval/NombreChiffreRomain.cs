e0e146ddfce53966fbc7ad7326ca21d3d190d8ca
Jeannegbl <jeanne.gablin@gmail.com>
2022-12-09 10:14:12 +0100

    refacto de la class en switch case

--- NombreRomainEval/NombreChiffreRomain.cs
+++ NombreRomainEval/NombreChiffreRomain.cs
@@ -7,17 +7,14 @@ public class NombreChiffreRomain
 
     public static string Nombre(int nombre)
     {
-        if (nombre == 1)
-            return Unite;
-        if (nombre == 2)
-            return Unite + Unite;
-        else if (nombre == 3)
-            return Unite + Unite + Unite;
-        else if (nombre == 4)
-            return Unite + Cinq;
-        else if (nombre == 5)
-            return Cinq;
-        else
-            throw new NotImplementedException();
+        return nombre switch
+        {
+            1 => Unite,
+            2 => Unite + Unite,
+            3 => Unite + Unite + Unite,
+            4 => Unite + Cinq,
+            5 => Cinq,
+            _ => throw new NotImplementedException(),
+        };
     }
 }
