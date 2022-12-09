780766361a6fbd6752e53404a1e3383b74cfa7f7
Jeannegbl <jeanne.gablin@gmail.com>
2022-12-09 11:46:06 +0100

    refacto du test

--- NombreRomainEval.Test/NombreRomainTest.cs
+++ NombreRomainEval.Test/NombreRomainTest.cs
@@ -8,30 +8,36 @@ namespace NombreRomainEval.Test
         [InlineData(1)]
         [InlineData(2)]
         [InlineData(3)]
+        [InlineData(4)]
         [InlineData(5)]
         [InlineData(6)]
         [InlineData(7)]
         [InlineData(8)]
+        [InlineData(9)]
         [InlineData(10)]
         [InlineData(11)]
         [InlineData(12)]
         [InlineData(13)]
+        [InlineData(14)]
         [InlineData(15)]
         [InlineData(16)]
         [InlineData(17)]
         [InlineData(18)]
+        [InlineData(19)]
         [InlineData(20)]
         [InlineData(21)]
         [InlineData(22)]
         [InlineData(23)]
+        [InlineData(24)]
         [InlineData(25)]
         [InlineData(26)]
         [InlineData(27)]
         [InlineData(28)]
+        [InlineData(29)]
         [InlineData(30)]
         public void Nombres(int n)
         {
-            // ETANT DONNER un nombre <n> compris entre 1 et 30 sans 4, 9, 14, 19, 24 et 29
+            // ETANT DONNER un nombre <n> compris entre 1 et 30
 
             // QUAND on le convertit en nombres romains
             var resultat = NombreRomain.Convertir(n);
@@ -40,6 +46,17 @@ namespace NombreRomainEval.Test
             {
                 nb = nb - 10;
                 nombre = nombre + "X";
+            }
+
+            if (nb == 9)
+            {
+                nb = nb - 9;
+                nombre = nombre + "IX";
+            }
+            if (nb == 4)
+            {
+                nb = nb - 4;
+                nombre = nombre + "IV";
             }
             while (nb >= 5)
             {
@@ -53,93 +70,8 @@ namespace NombreRomainEval.Test
                 nombre = nombre + "I";
             }
 
-            // ALORS on obtient son nombre romain comprit entre I et XXX sans IV, IX, XIV, XIX, XXIV et XXIX
+            // ALORS on obtient son nombre romain comprit entre I et XXX
             var attendu = nombre;
-
-            Assert.Equal(attendu, resultat);
-        }
-
-        [Fact]
-        public void Nombre4()
-        {
-            // ETANT DONNEE le nombre 4
-            const int nombre = 4;
-
-            // QUAND on le convertit en nombres romains
-            var resultat = NombreRomain.Convertir(nombre);
-
-            // ALORS on obtient le nombre IV
-            var attendu = "IV";
-
-            Assert.Equal(attendu, resultat);
-        }
-        [Fact]
-        public void Nombre9()
-        {
-            // ETANT DONNEE le nombre 9
-            const int nombre = 9;
-
-            // QUAND on le convertit en nombres romains
-            var resultat = NombreRomain.Convertir(nombre);
-
-            // ALORS on obtient le nombre IX
-            var attendu = "IX";
-
-            Assert.Equal(attendu, resultat);
-        }
-        [Fact]
-        public void Nombre14()
-        {
-            // ETANT DONNEE le nombre 14
-            const int nombre = 14;
-
-            // QUAND on le convertit en nombres romains
-            var resultat = NombreRomain.Convertir(nombre);
-
-            // ALORS on obtient le nombre XIV
-            var attendu = "XIV";
-
-            Assert.Equal(attendu, resultat);
-        }
-        [Fact]
-        public void Nombre19()
-        {
-            // ETANT DONNEE le nombre 19
-            const int nombre = 19;
-
-            // QUAND on le convertit en nombres romains
-            var resultat = NombreRomain.Convertir(nombre);
-
-            // ALORS on obtient le nombre XIX
-            var attendu = "XIX";
-
-            Assert.Equal(attendu, resultat);
-        }
-        [Fact]
-        public void Nombre24()
-        {
-            // ETANT DONNEE le nombre 24
-            const int nombre = 24;
-
-            // QUAND on le convertit en nombres romains
-            var resultat = NombreRomain.Convertir(nombre);
-
-            // ALORS on obtient le nombre XXIV
-            var attendu = "XXIV";
-
-            Assert.Equal(attendu, resultat);
-        }
-        [Fact]
-        public void Nombre29()
-        {
-            // ETANT DONNEE le nombre 29
-            const int nombre = 29;
-
-            // QUAND on le convertit en nombres romains
-            var resultat = NombreRomain.Convertir(nombre);
-
-            // ALORS on obtient le nombre XXIV
-            var attendu = "XXIX";
 
             Assert.Equal(attendu, resultat);
         }
